1935fe92e5a22ef72e86f4ec9fe1dbcfbf55fdaf
HoodaHashem <mahmoudhashem335@gmail.com>
2024-12-02 01:32:38 +0200

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,112 @@
+const logotext = ". MAHMOUD";
+
+const meta = {
+  title: "Mahmoud",
+  description:
+    "I’m Mahmoud Hashem software engineer_ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Mahmoud Hashem",
+  animated: {
+    first: "I'M SOFTWARE ENGINEER",
+    second: "I LOVE WEBSITES",
+    third: "I LOVE COFFEE",
+  },
+  description:
+    "I am a software engineer specializing in full-stack development, creating scalable, user-focused solutions with modern technologies like Node.js, React, and Python.",
+  your_img_url: "./me.jpg",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I am a passionate software engineer with expertise in full-stack development, specializing in creating scalable web applications and efficient backend systems. Proficient in modern technologies like Node.js, React, Python, and Rust, I have a strong foundation in problem-solving, system design, and performance optimization. My work focuses on building user-centric solutions, leveraging clean code principles, and continuously exploring innovative tools to enhance software reliability and maintainability.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "Djano",
+    value: 85,
+  },
+  {
+    name: "Flask",
+    value: 50,
+  },
+  {
+    name: "NodeJS",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "ExpressJS",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "Full Stack Development",
+    description:
+      "Design and build complete web applications, from backend APIs to frontend user interfaces, using technologies like Node.js, React, and MongoDB. Offer tailored solutions for businesses to streamline their operations or enhance their online presence.",
+  },
+  {
+    title: "Frontend Development",
+    description:
+      "Crafting modern, responsive, and visually engaging user interfaces using the latest technologies to ensure seamless user experiences across devices.",
+  },
+  {
+    title: "Backend Development",
+    description:
+      "Building secure, scalable, and efficient server-side solutions that power applications and handle complex data workflows.",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "mahmoudhashem335@gmail.com",
+  YOUR_FONE: "+20 127 6556 853",
+  description:
+    "Whether you have a question, project in mind, or just want to connect, I’m here to help. Feel free to reach out, and I'll get back to you as soon as possible. Let’s discuss how we can work together to bring your ideas to life!",
+};
+
+const socialprofils = {
+  github: "https://github.com/HoodaHashem",
+  linkedin:
+    "https://www.linkedin.com/in/%E2%80%AAmahmoud-hashem%E2%80%AC%E2%80%8F-b85480250/",
+  twitter: "https://x.com/MahmoudHashem26",
+};
+export {
+  meta,
+  dataabout,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
