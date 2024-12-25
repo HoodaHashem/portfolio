29398b1f078f5524e9d8f494433e46afe056fb19
HoodaHashem <mahmoudhashem335@gmail.com>
2024-12-25 21:22:53 +0200

    style: styling the footer

--- src/components/sections/footer/index.jsx
+++ src/components/sections/footer/index.jsx
@@ -1,3 +1,9 @@
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaWhatsapp,
+  FaXTwitter,
+} from "react-icons/fa6";
 import "./index.css";
 const Footer = () => {
   return (
@@ -10,11 +16,33 @@ const Footer = () => {
           </div>
         </div>
         <div className="footer-links">
-          <a href="/terms" className="footer-link">
-            Terms and Conditions
+          <a
+            href="https://github.com/HoodaHashem"
+            className="social-link"
+            target="_blank"
+          >
+            <FaGithub />
           </a>
-          <a href="/privacy" className="footer-link">
-            Privacy and Policy
+          <a
+            className="social-link"
+            href="https://x.com/HoodaHashem"
+            target="_blank"
+          >
+            <FaXTwitter />
+          </a>
+          <a
+            className="social-link"
+            href="https://wa.me/+201551028992"
+            target="_blank"
+          >
+            <FaWhatsapp />
+          </a>
+          <a
+            className="social-link"
+            href="https://www.linkedin.com/in/hooda-hashem"
+            target="_blank"
+          >
+            <FaLinkedinIn />
           </a>
         </div>
       </div>
