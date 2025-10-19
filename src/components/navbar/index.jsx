4e89a0f9dfc28b2f48ae11ae8b1c00bd9b2ace54
HoodaHashem <mahmoud.hashem.dev@gmail.com>
2025-10-19 20:27:54 +0300

    Fix: fixing navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import PrimaryBtn from "../primaryBtn";
 import "./index.css";
 import { useState } from "react";
 
@@ -43,56 +42,6 @@ const Navbar = () => {
         <div className="navImgBox">
           <img className="navLogo" src="./navlogo.svg" alt="Logo" />
         </div>
-        <label className="hamburger">
-          <input
-            type="checkbox"
-            checked={hamMenu}
-            onChange={() => setHamMenu(!hamMenu)}
-          />
-          <svg viewBox="0 0 32 32">
-            <path
-              className="line line-top-bottom"
-              d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
-            ></path>
-            <path className="line" d="M7 16 27 16"></path>
-          </svg>
-        </label>
-        <div className={`hamMenu ${hamMenu ? "open" : ""}`}>
-          <ul className="navbarList">
-            <li className="navLink">
-              <a href="#hero" onClick={() => setHamMenu(false)}>
-                <p className="activeText">Home</p>
-              </a>
-            </li>
-            <li className="navLink">
-              <a href="#about" onClick={() => setHamMenu(false)}>
-                <p>About Me</p>
-              </a>
-            </li>
-            <li className="navLink">
-              <a href="#portfolio" onClick={() => setHamMenu(false)}>
-                <p>Portfolio</p>
-              </a>
-            </li>
-            <li className="navLink">
-              <a href="#contact" onClick={() => setHamMenu(false)}>
-                <p>Contact me</p>
-              </a>
-            </li>
-            <li className="nav-btn">
-              <PrimaryBtn
-                onClick={() => {
-                  document
-                    .getElementById("contact")
-                    ?.scrollIntoView({ behavior: "smooth" });
-                  setHamMenu(false);
-                }}
-              >
-                <p>Hire Me</p>
-              </PrimaryBtn>
-            </li>
-          </ul>
-        </div>
       </div>
     );
   }
@@ -123,15 +72,6 @@ const Navbar = () => {
           </a>
         </li>
       </ul>
-      <PrimaryBtn
-        onClick={() => {
-          document
-            .getElementById("contact")
-            ?.scrollIntoView({ behavior: "smooth" });
-        }}
-      >
-        <p>Hire Me</p>
-      </PrimaryBtn>
     </div>
   );
 };
